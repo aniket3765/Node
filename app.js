434a21815d8f673f360aec6b0bbdf2445a80081b
aniket3765 <121852693+aniket3765@users.noreply.github.com>
2023-06-07 15:37:03 +0530

    parse body and limiting middleware- app.js

--- app.js
+++ app.js
@@ -11,18 +11,45 @@
 //const server = http.createServer(route)
 
 //server.listen(4000);
+// const http = require('http');
+// const express = require('express');
+
+// const app = express();
+
+
+
+// app.use((req, res, next)=>{
+//     console.log('middleware');
+//     next();
+// });
+
+// app.use((req, res, next)=>{
+//     console.log('middleware 2');
+//     res.send('<h1>hello Express</h1>')
+// });
+
+// app.listen(3000);
+
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log('middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add', (req, res, next) => {
+   
+    res.send('<form action="/product" method="post"><input type="text" name="title"><button type="submit"></form>')
 });
 
-app.use((req, res, next)=>{
-    console.log('middleware 2');
+app.post('/product',(req, res, next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+  
     res.send('<h1>hello Express</h1>')
 });
 

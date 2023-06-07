7e25f050a2b90507bc7267e256609da8b9253e65
aniket3765 <121852693+aniket3765@users.noreply.github.com>
2023-06-07 14:57:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,24 @@
 
 //server.listen(4000);
 
-const http  = require('http');
-const route = require('./route.js')
-const server = http.createServer(route)
+//const http  = require('http');
+//const route = require('./route.js')
+//const server = http.createServer(route)
 
-server.listen(4000);
+//server.listen(4000);
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log('middleware');
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log('middleware 2');
+    res.send('<h1>hello Express</h1>')
+});
+
+app.listen(3000);
